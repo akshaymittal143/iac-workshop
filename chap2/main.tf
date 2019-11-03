0a32caebb20d9a3b300bf1e2fb55cc495bbf772d
Akshay Mittal <akshay.mittal@coxautoinc.com>
2019-11-03 17:43:24 -0600

    update chap2

--- chap2/main.tf
+++ chap2/main.tf
@@ -1,6 +1,8 @@
 ##################################################################################
 # VARIABLES
 ##################################################################################
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 variable "private_key_path" {}
 variable "key_name" {}
 variable "region" {
@@ -18,6 +20,8 @@ variable "subnet1_address_space" {
 ##################################################################################
 
 provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region = var.region
 }
 
--- chap2/terraform.tfvars
+++ chap2/terraform.tfvars
@@ -1,2 +1,4 @@
+aws_access_key="ENTER_YOUR_KEY_HERE"
+aws_secret_key="ENTER_YOUR_SECRET_HERE"
 key_name="MyKeyPair"
 private_key_path="./MyKeyPair.pem"
