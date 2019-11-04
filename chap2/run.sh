1253bf9447130f2186b6cfe6271168429ac46745
Akshay Mittal <akshay.mittal@coxautoinc.com>
2019-11-03 18:29:47 -0600

    update run.sh in chap2

--- chap2/run.sh
+++ chap2/run.sh
@@ -1,8 +1,13 @@
-ssh-keygen -f ~/.ssh/aws
+# ssh-keygen -f ~/.ssh/aws
+# you will need aws_access_key & aws_secret_key, region: us-east-2
+aws configure
 
-aws ec2 create-key-pair --key-name MyKeyPair --query \
-'KeyMaterial' --output text > MyKeyPair.pem
+# this will create ec2 key-pair name MyKeyPair and download MyKeyPair.pem file in chap2 dir
+aws ec2 create-key-pair --region us-east-2 --key-name MyKeyPair --query 'KeyMaterial' --output text > MyKeyPair.pem
 
+# if you see: An error occurred (InvalidKeyPair.Duplicate) when calling the CreateKeyPair operation: The keypair 'MyKeyPair' already exists.
+# it means you already have MyKeyPair created so you may want to change --key-name example MyKeyPair1, make sure change in terraform.tfvars too
 ls
 
+# providing read permission
 chmod 400 MyKeyPair.pem
