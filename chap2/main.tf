b7e8a1f121b810ea5e26a633f3fdcbbee09b2dc6
Akshay Mittal <akshay.mittal@coxautoinc.com>
2019-11-07 13:33:07 -0600

    update chap2 region

--- chap2/main.tf
+++ chap2/main.tf
@@ -6,7 +6,7 @@ variable "aws_secret_key" {}
 variable "private_key_path" {}
 variable "key_name" {}
 variable "region" {
-  default = "us-east-1"
+  default = "us-east-2"
 }
 variable "network_address_space" {
   default = "10.1.0.0/16"
